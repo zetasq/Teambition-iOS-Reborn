c3a7f8f15820ab64f6f3230b4a383da210101299
Zhu Shengqi <zetasq@gmail.com>
2016-12-24 16:41:25 +0800

    Project setup.

---
+++ Source/MainInterface/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  Tinbox
+//
+//  Created by Zhu Shengqi on 24/12/2016.
+//  Copyright © 2016 Zhu Shengqi. All rights reserved.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
